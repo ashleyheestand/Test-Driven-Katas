ceef4930a422923cd92b4b537b5273dce7442a2b
Ashley Heestand <heestand.ashley@gmail.com>
2019-03-14 10:37:18 -0500

    broken thing

--- wrap.js
+++ wrap.js
@@ -1,3 +1,22 @@
-function wrap (string, colNum) {
-    
-}
+const wrap = (line, maxLen) => {
+  if (line.length > maxLen) {
+    const indexOfBlank = line.lastIndexOf(' ', maxLen);
+    let split;
+    let offset;
+    if (indexOfBlank > -1) {
+      split - indexOfBlank;
+      offset = 1;
+    } else {
+      split = maxLen;
+      offset = 0;
+    }
+    return (
+      line.substring(0, split) +
+      '\n' +
+      wrap(line.substring(split + offset), maxLen)
+    );
+  }
+  return line;
+};
+
+module.exports = wrap;
--- wrap.spec.js
+++ wrap.spec.js
@@ -3,6 +3,12 @@ const wrap = require('./wrap');
 
 describe('wrap', () => {
   it('Returns empty string if empty string was provided', () => {
-    expect(wrap("", 10)).to.equal("");
+    expect(wrap('', 10)).to.equal('');
+  });
+  it('Returns the string if the string length is less than the maxLen', () => {
+    expect(wrap('hello', 10)).to.equal('hello');
+  });
+  it('Does not return a single line if the string is longer than the maxLen', () => {
+    expect(wrap('this is a test', 5)).to.equal('this \nis a \ntest');
   });
 });
